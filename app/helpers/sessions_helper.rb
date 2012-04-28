17ac0d8dc1b7319eef1aaf487b7f78b13d4fb946
Eric Galluzzo <egalluzzo@gmail.com>
2012-04-28 14:16:17 -0400

    Exercises from Chapter 8 (except number 3)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,8 @@
 module SessionsHelper
 
   def sign_in(user)
-    cookies.permanent[:remember_token] = user.remember_token
+    #cookies.permanent[:remember_token] = user.remember_token
+    session[:remember_token] = user.remember_token
     current_user = user
   end
 
@@ -19,13 +20,15 @@ module SessionsHelper
 
   def sign_out
     current_user = nil
-    cookies.delete(:remember_token)
+    #cookies.delete(:remember_token)
+    session.delete(:remember_token)
   end
 
   private
 
     def user_from_remember_token
-      remember_token = cookies[:remember_token]
+      #remember_token = cookies[:remember_token]
+      remember_token = session[:remember_token]
       User.find_by_remember_token(remember_token) unless remember_token.nil?
     end
 end
